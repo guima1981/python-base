4957065554ad88bd5821b9cfaa38cb76903e08e2
Marcelo Guimaraes <mfguimaraes@gmail.com>
2023-08-22 12:26:04 -0300

    news beginning

--- hello.py
+++ hello.py
@@ -14,15 +14,31 @@ Execução:
     ou
     ./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __autor__="Marcelo Guimaraes"
 __license__="Unlicense"
 
 import os
+# Execução: LANG=fr_FR ./hello.py
 
-#current_language = "en_US"
-current_language = os.getenv("LANG","en_US")[:5]
+current_language = "en_US"
+#current_language = os.getenv("LANG","en_US")[:5]
 
+# 01
+# Sets (Hash Table) - O(1) - Constante
+# dicts (Hash Table)
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_SP": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Monde!",
+}
+print(msg[current_language])
+
+
+# 02
+# Ordem de Complexidade O(n) 
 msg = "Hello, World!"
 
 if current_language == "pt_BR":
